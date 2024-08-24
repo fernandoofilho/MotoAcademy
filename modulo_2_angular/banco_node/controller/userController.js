a53d9a0a8002b1b2beaec58cf214ff64a097be68
fernando <faaf.eng19@uea.edu.br>
2024-08-23 20:24:35 -0400

    criacao de usuario

--- modulo_2_angular/banco_node/controller/userController.js
+++ modulo_2_angular/banco_node/controller/userController.js
@@ -9,11 +9,18 @@ async function getUsers(req, res){
     }
 
 };
-function createUser(req, res){
+async function createUser(req, res){
     try {
-        res.status(200).send({message: "Hello World"})
-    } catch (error) {
+
+        const {nome, idade, email, senha } = req.body;
         
+        
+        const users = await usersService.createUser(nome,idade,email,senha)
+        return res.status(200).json(req.body);
+
+    } catch (error) {
+                res.status(400).send({ message: "Erro ao criar novo usuário" });
+
     }
 
 };
--- modulo_2_angular/banco_node/services/userService.js
+++ modulo_2_angular/banco_node/services/userService.js
@@ -7,5 +7,11 @@ async function getAllUsers() {
     return results
 }   
 
-
-module.exports = {getAllUsers};
+async function createUser(nome, idade, email, senha) {
+    const conn = await mysql2.createConnection(config);
+    const sqlCreate = 'INSERT INTO `aluno` (`nome`, `idade`, `email`,  `senha`) VALUES(?, ?, ?, ?)'
+    const values = [nome, idade, email, senha]
+    const [results] = await conn.execute(sqlCreate, values);
+    return results;
+}
+module.exports = { getAllUsers, createUser };
